4c9ba7b6bc5454de6d0eb9de15365284072273ba
Titanoboba <nik20052006@mail.ru>
2024-05-19 09:15:37 -0400

    Updated CPack

--- CPack.cmake
+++ CPack.cmake
@@ -2,9 +2,9 @@ include(InstallRequiredSystemLibraries)
 
 set(CPACK_PACKAGE_CONTACT eisnersandy2@gmail.com)
 set(CPACK_PACKAGE_VERSION )
-set(CPACK_PACKAGE_NAME "reshala")
+set(CPACK_PACKAGE_NAME "solver")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "C++ library")
-set(CPACK_PACKAGE_PACK_NAME "reshala-")
+set(CPACK_PACKAGE_PACK_NAME "solver-")
 
 set(CPACK_SOURCE_INSTALLED_DIRECTORIES 
   "/solver_application; solver_application"
@@ -12,7 +12,7 @@ set(CPACK_SOURCE_INSTALLED_DIRECTORIES
   "/formatter_ex_lib; formatter_ex_lib"
   "/formatter_lib; formatter_lib")
 
-set(CPACK_RESOURCE_FILE_README /README.md)
+set(CPACK_RESOURCE_FILE_README README.md)
 
 set(CPACK_SOURCE_GENERATOR "TGZ;ZIP")
 
@@ -21,7 +21,7 @@ set(CPACK_DEBIAN_PACKAGE_RELEASE 1)
 
 set(CPACK_DEBIAN_PACKAGE_VERSION )
 set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "all")
-set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "reshaet")
+set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "it's solving")
 
 set(CPACK_GENERATOR "DEB;RPM")
 
